97064fed2f75359c9e8b4e08c30e0d020063e2a5
jasimkareem <147159170+Jasimkareem04@users.noreply.github.com>
2024-08-15 21:19:11 +0530

    Player turn

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,35 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    
-    public Vector2 Pos = new Vector2(338, 327);
-    bool IsClicked = false;
-    Text xText;
-    Text oText;
-    public Text[] PlayerTxt;
+    public int WhoseTurn = 0; //0 Xturn 1 Oturn
+    public Sprite[] PlayerImage;
+    public Button[] buttons;
+    // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.DeleteKey("Txt");
-        xText.text = "X";
-        oText.text = "O";
+        GameSetup();  
     }
+    void GameSetup()
+    {
+        WhoseTurn = 0;
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].enabled = true;
+            buttons[i].GetComponent<Image>().sprite = null;
+        }
+    }
+
+    public void OnClick(int whichBtn)
+    {
+        buttons[whichBtn].image.sprite = PlayerImage[WhoseTurn];
+        buttons[whichBtn].enabled = false;
+
+        if (WhoseTurn == 0)
+        {
+            WhoseTurn = 1;
+        }
+        else
+        {
+            WhoseTurn = 0;
+        }
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        PlayerTxt[1].text = PlayerPrefs.GetString("Txt");
+
     }
-    public void Onclick()
-    {
-        if (!IsClicked)
-        {
-            PlayerPrefs.SetString("Txt","X");
-            IsClicked = true;
-        }
-        
-    }
+   
 }
