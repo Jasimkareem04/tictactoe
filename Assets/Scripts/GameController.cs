3fcd49166ef1b9af0b55829567b7312087a2e6d6
jasimkareem <147159170+Jasimkareem04@users.noreply.github.com>
2024-08-26 01:02:46 +0530

    created BotLvl2
    Now there is easy and medium level of hardness while playing with computer

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,6 +14,9 @@ public class GameController : MonoBehaviour
     public GameObject WinnerPanel;
     public Text WinnerTxt;
     public static bool BotTurn;
+    public static bool BotTurn2;
+    int s1, s2, s3, s4, s5, s6, s7, s8;
+    public int[] solution;
 
 
     // Start is called before the first frame update
@@ -20,7 +24,7 @@ public class GameController : MonoBehaviour
     {
         GameSetup();
     }
-    void GameSetup() 
+    public void GameSetup() 
     {
         WhoseTurn = 0;
         turncount = 0;
@@ -52,6 +56,10 @@ public class GameController : MonoBehaviour
         {
             bot();
         }
+        else if(BotTurn2 == true)
+        {
+            botLvl2();
+        }
         else if (BotTurn == false && WhoseTurn == 0)
         {
             WhoseTurn = 1;
@@ -65,15 +73,15 @@ public class GameController : MonoBehaviour
 
     public void checkWinner()
     {
-        int s1 = markedSpace[0] + markedSpace[1] + markedSpace[2];
-        int s2 = markedSpace[3] + markedSpace[4] + markedSpace[5];
-        int s3 = markedSpace[6] + markedSpace[7] + markedSpace[8];
-        int s4 = markedSpace[0] + markedSpace[3] + markedSpace[6];
-        int s5 = markedSpace[1] + markedSpace[4] + markedSpace[7];
-        int s6 = markedSpace[2] + markedSpace[5] + markedSpace[8];
-        int s7 = markedSpace[0] + markedSpace[4] + markedSpace[8];
-        int s8 = markedSpace[2] + markedSpace[4] + markedSpace[6];
-        var solution = new int[] { s1, s2, s3, s4, s5, s6, s7, s8 };
+        s1 = markedSpace[0] + markedSpace[1] + markedSpace[2];
+        s2 = markedSpace[3] + markedSpace[4] + markedSpace[5];
+        s3 = markedSpace[6] + markedSpace[7] + markedSpace[8];
+        s4 = markedSpace[0] + markedSpace[3] + markedSpace[6];
+        s5 = markedSpace[1] + markedSpace[4] + markedSpace[7];
+        s6 = markedSpace[2] + markedSpace[5] + markedSpace[8];
+        s7 = markedSpace[0] + markedSpace[4] + markedSpace[8];
+        s8 = markedSpace[2] + markedSpace[4] + markedSpace[6];
+        solution = new int[] { s1, s2, s3, s4, s5, s6, s7, s8 };
         for (int i = 0; i < solution.Length; i++)
         {
             if (solution[i] == 3 * (WhoseTurn + 1)) //check 3 spaces contains same icon
@@ -107,7 +115,7 @@ public class GameController : MonoBehaviour
             if(markedSpace[i] == -10)
             {                  
                 buttons[i].image.sprite = PlayerImage[WhoseTurn];
-                buttons[i].interactable = false;
+                buttons[i].enabled = false;
                 markedSpace[i] = WhoseTurn + 1;
                 checkWinner();
                 turncount++;                
@@ -115,6 +123,113 @@ public class GameController : MonoBehaviour
                 break;
             }
         }
+    }
+    public void botLvl2()
+    {
+        bool blocked = false;
+        checkWinner();
+           for (int i = 0; i < solution.Length; i++)
+           {           
+              if (solution[i] == -8) //check 2 spaces contains Xicon
+              {
+                    blocked = true;
+                    Debug.Log("Bot2 found");
+                    WhoseTurn = 1;
+                    switch(i)
+                    {
+                        case 0:
+                            for (int j = 0;j<3;j++)
+                               {
+                                if (markedSpace[j]==-10)
+                                {
+                                 blockPlayer(j); 
+                                }
+                               }
+                               break;
+                        case 1:
+                            for (int j = 3; j < 6; j++)
+                            {
+                                if (markedSpace[j] == -10)
+                                {
+                                    blockPlayer(j);
+                                }
+                            }
+                            break;
+                        case 2:
+                            for (int j = 6; j < 9; j++)
+                            {
+                                if (markedSpace[j] == -10)
+                                {
+                                    blockPlayer(j);
+                                }
+                            }
+                            break;
+                        case 3:
+                            for (int j = 0; j < 7; j=j+3)
+                            {
+                                if (markedSpace[j] == -10)
+                                {
+                                    blockPlayer(j);
+                                }
+                            }
+                            break;
+                        case 4:
+                            for (int j = 1; j < 8; j=j+3)
+                            {
+                                if (markedSpace[j] == -10)
+                                {
+                                    blockPlayer(j);
+                                }
+                            }
+                            break;
+                        case 5:
+                            for (int j = 2; j < 9; j = j + 3)
+                            {
+                                if (markedSpace[j] == -10)
+                                {
+                                    blockPlayer(j);
+                                }
+                            }
+                            break;
+                        case 6:
+                            for (int j = 0; j < 9; j = j + 4)
+                            {
+                                if (markedSpace[j] == -10)
+                                {
+                                    blockPlayer(j);
+                                }
+                            }
+                            break;
+                        case 7:
+                            for (int j = 2; j < 7; j = j + 2)
+                            {
+                                if (markedSpace[j] == -10)
+                                {
+                                    blockPlayer(j);
+                                }
+                            }
+                            break;
+                        default: Debug.Log("default");
+                            break;
+
+
+                    }
+                    break;
+              }
+           }
+       if(!blocked)
+       {
+         bot();
+       }
+    }
+    public void blockPlayer(int j)
+    {
+        buttons[j].image.sprite = PlayerImage[WhoseTurn];
+        buttons[j].enabled = false;
+        markedSpace[j] = WhoseTurn + 1;
+        checkWinner();
+        turncount++;
+        WhoseTurn = 0;
     }
 }
 
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Button EasyBtn;
+    public Button MediumBtn;
+
+
+    public void Start()
+    {
+        EasyBtn.gameObject.SetActive(false);
+        MediumBtn.gameObject.SetActive(false);
+    }
     public void MultiPlayer()
     {
         SceneManager.LoadSceneAsync(0);
         GameController.BotTurn = false;
+        
     }
     public void SinglePlayer()
     {
+        EasyBtn.gameObject.SetActive(true);
+        MediumBtn.gameObject.SetActive(true);
+    }
+    public void Medium()
+    {
         SceneManager.LoadSceneAsync(0);
-        GameController.BotTurn = true;
+        GameController.BotTurn2 = true;
+        GameController.BotTurn = false;
+    }
+    public void Easy()
+    {
+       SceneManager.LoadSceneAsync(0);
+       GameController.BotTurn = true;
+        GameController.BotTurn2 = false;
     }
 }
